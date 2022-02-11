9cb28dbb31919111962a301b6d116d08de5a5d47
ACLatte <c0015843@newcastle.ac.uk>
2022-02-11 00:08:59 +0000

    delete method in the pacemaker,yang

--- src/main/java/uk/ac/ncl/controller/pacemaker.java
+++ src/main/java/uk/ac/ncl/controller/pacemaker.java
@@ -1,22 +1,11 @@
 package uk.ac.ncl.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
 /**
  * @author Yang
  * @date 2022/2/10
  */
 @RestController
 public class pacemaker {
-
-    //run the page localhost:8080
-    //it will auto skip to index.html
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public ModelAndView pacemakerSimulater(){
-        return new ModelAndView("index");
-    }
 
 }
