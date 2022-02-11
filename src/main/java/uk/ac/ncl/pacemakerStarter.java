1e118b51ed7747aff2fa8136b979da47ccaeb598
ACLatte <c0015843@newcastle.ac.uk>
2022-02-11 00:06:37 +0000

    elementui+vue+echarts,yang

---
+++ src/main/java/uk/ac/ncl/pacemakerStarter.java
@@ -0,0 +1,11 @@
+package uk.ac.ncl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pacemakerStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(pacemakerStarter.class, args);
+    }
+}
