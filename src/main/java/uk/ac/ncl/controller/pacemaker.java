8bd621855f2ddbf9764d0676184d6917692a28a2
ACLatte <c0015843@newcastle.ac.uk>
2022-02-11 00:20:21 +0000

    add some annotation for entity project,yang

--- src/main/java/uk/ac/ncl/controller/pacemaker.java
+++ src/main/java/uk/ac/ncl/controller/pacemaker.java
@@ -1,11 +1,63 @@
 package uk.ac.ncl.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author Yang
  * @date 2022/2/10
  */
+//this is contains all simulated echarts methods
 @RestController
 public class pacemaker {
 
+    //control pacemaker whether open
+    @RequestMapping("/pacemakerSwitch")
+    public List pacemakerSwitch(){
+
+        return null;
+    }
+
+    //reset heartRate parameter to default value
+    @RequestMapping("/resSet")
+    public List resSet(){
+
+        return null;
+    }
+
+
+    //adjust slider of HeartRate to change echarts
+    @RequestMapping("/heartRate")
+    public List heartRate(){
+
+        return null;
+    }
+
+    //adjust slider of PRInterval to change echarts
+    @RequestMapping("/PRInterval")
+    public List PRInterval(){
+
+        return null;
+    }
+
+    //choice different button to change different mode
+    @RequestMapping("/modeAAI")
+    public List modeAAI(){
+
+        return null;
+    }
+
+    @RequestMapping("/modeVDD")
+    public List modeVDD(){
+
+        return null;
+    }
+
+    @RequestMapping("/modeDDD")
+    public List modeDDD(){
+
+        return null;
+    }
 }
--- src/main/java/uk/ac/ncl/pacemakerStarter.java
+++ src/main/java/uk/ac/ncl/pacemakerStarter.java
@@ -3,6 +3,11 @@ package uk.ac.ncl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Yang
+ * @date 2022/2/10
+ */
+//run this starter to running entity progress
 @SpringBootApplication
 public class pacemakerStarter {
     public static void main(String[] args) {
