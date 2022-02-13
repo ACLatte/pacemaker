0d47b8b84bdfea3b6150efb2dfca7f379c8588fe
ACLatte <c0015843@newcastle.ac.uk>
2022-02-13 19:03:18 +0000

    add some annotation in the index.html,yang

--- src/main/java/uk/ac/ncl/pojo/avNode.java
+++ src/main/java/uk/ac/ncl/pojo/avNode.java
@@ -1,9 +1,9 @@
 package uk.ac.ncl.pojo;
 
 public class avNode {
-    private final Integer firstPoint = 55;
+    private final Integer firstPoint = 60;
     private final Integer middlePoint = 65;
-    private final Integer lastPoint = 55;
+    private final Integer lastPoint = 60;
 
     public Integer getFirstPoint() {
         return firstPoint;
